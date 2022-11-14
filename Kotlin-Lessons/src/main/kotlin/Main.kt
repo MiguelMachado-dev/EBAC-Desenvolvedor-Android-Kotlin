3a8ce48ae114742c0fef7b1bad702500b1e5739c
Miguel Machado <miiguellmp@gmail.com>
2022-11-14 00:10:57 -0300

    feat: High order functions

--- Kotlin-Lessons/src/main/kotlin/Main.kt
+++ Kotlin-Lessons/src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    firstHighOrderFunction("Hello ", "world", ::concatString)
+    firstHighOrderFunction("Another", "text", ::printStringNumberOfLetters)
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun printStringNumberOfLetters(val1: String, val2: String) = println("$val1 has ${val1.length} letters and $val2 has ${val2.length} letters")
+
+fun concatString(val1: String, val2: String) = println(val1 + val2)
+
+fun firstHighOrderFunction(text1: String, text2: String, operation: (String, String) -> Unit) {
+    operation(text1, text2)
 }
