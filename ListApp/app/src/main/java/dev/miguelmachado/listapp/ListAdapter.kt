48daea068a372de2d07f21f95708ca0e7ab7f010
Miguel Machado <miiguellmp@gmail.com>
2022-11-29 12:38:55 -0300

    refactor: usando viewBinding - Substituir o R.id

--- ListApp/app/src/main/java/dev/miguelmachado/listapp/ListAdapter.kt
+++ ListApp/app/src/main/java/dev/miguelmachado/listapp/ListAdapter.kt
@@ -1,17 +1,21 @@
 package dev.miguelmachado.listapp
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import dev.miguelmachado.listapp.databinding.ItemBinding
 
 class ListAdapter : RecyclerView.Adapter<ListAdapter.ListItemViewHolder>() {
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
-    val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
-
-    return ListItemViewHolder(view)
+    return ListItemViewHolder(
+      ItemBinding.inflate(
+        LayoutInflater.from(parent.context),
+        parent,
+        false
+      )
+    )
   }
 
   override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
@@ -23,8 +27,9 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.ListItemViewHolder>() {
     return 1
   }
 
-  class ListItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    var position: TextView = view.findViewById(R.id.posicao)
-    var content: TextView = view.findViewById(R.id.conteudo)
+  // binding.root é a raiz do layout item.xml - que é a view que o RecyclerView vai usar para exibir
+  class ListItemViewHolder(binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    var position: TextView = binding.posicao
+    var content: TextView = binding.conteudo
   }
 }
