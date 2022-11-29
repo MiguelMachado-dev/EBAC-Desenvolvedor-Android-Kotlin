88faf20011b13f807f08a92761e8b34e4ff75f6b
Miguel Machado <miiguellmp@gmail.com>
2022-11-29 19:37:13 -0300

    feat: viewbinding pratica - Aula 4

---
+++ ListApp/app/src/main/java/dev/miguelmachado/listapp/City.kt
@@ -0,0 +1,6 @@
+package dev.miguelmachado.listapp
+
+data class ListItem(
+  val city: String,
+  val state: String,
+)
--- ListApp/app/src/main/java/dev/miguelmachado/listapp/ListAdapter.kt
+++ ListApp/app/src/main/java/dev/miguelmachado/listapp/ListAdapter.kt
@@ -1,12 +1,17 @@
 package dev.miguelmachado.listapp
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import dev.miguelmachado.listapp.databinding.ItemBinding
 
-class ListAdapter : RecyclerView.Adapter<ListAdapter.ListItemViewHolder>() {
+class ListAdapter(
+  val listItems: MutableList<ListItem>,
+  val listener: ListAdapterListener
+) :
+  RecyclerView.Adapter<ListAdapter.ListItemViewHolder>() {
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
     return ListItemViewHolder(
@@ -19,17 +24,27 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.ListItemViewHolder>() {
   }
 
   override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
+    val item = listItems[position]
     holder.position.text = position.toString()
-    holder.content.text = "Item $position"
+    holder.content.text = item.city
+
+    holder.root.setOnClickListener {
+      listener.onItemClicked(item.city)
+    }
   }
 
   override fun getItemCount(): Int {
-    return 1
+    return listItems.size
   }
 
   // binding.root é a raiz do layout item.xml - que é a view que o RecyclerView vai usar para exibir
   class ListItemViewHolder(binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
     var position: TextView = binding.posicao
     var content: TextView = binding.conteudo
+    var root: View = binding.root
+  }
+
+  interface ListAdapterListener {
+    fun onItemClicked(content: String);
   }
 }
